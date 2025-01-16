fee41551d8a4ebbf49db0c7f80fc76649d140fd4
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-16 13:27:29 -0500

    clear template and configure primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsFormComponent } from "./products/components/products-form/products-form.component";
+import { ProductsListComponent } from "./products/components/products-list/products-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsFormComponent, ProductsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
