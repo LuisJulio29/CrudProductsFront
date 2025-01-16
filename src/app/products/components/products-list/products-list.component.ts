47679165d6150d33433b91129d3dd7f17466205d
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-16 13:29:14 -0500

    add product list component

---
+++ src/app/products/components/products-list/products-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-products-list',
+  imports: [ButtonModule],
+  templateUrl: './products-list.component.html',
+  styleUrl: './products-list.component.css'
+})
+export class ProductsListComponent {
+
+}
