448e0aef245514e4cd9e628d14806f49b3bec942
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-18 14:12:41 -0500

    create and configuratede app routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(APP_ROUTES),
     provideAnimationsAsync(),
-    provideHttpClient(), provideAnimationsAsync()
+    provideHttpClient()
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PRODUCT_ROUTES } from './products/products.routes';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+    {path: '', redirectTo: 'products', pathMatch: 'full'},
+    {path:'products', children: PRODUCT_ROUTES}
+];
