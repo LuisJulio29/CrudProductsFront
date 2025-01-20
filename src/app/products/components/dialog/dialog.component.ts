c94f5428fa20517ac5bc5e3391b8a37c191cc75b
Luis E. Julio Padilla <ljuliop@unicartagena.edu.co>
2025-01-20 11:19:28 -0500

    component logic

--- src/app/products/components/dialog/dialog.component.ts
+++ src/app/products/components/dialog/dialog.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog',
-  imports: [],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
 export class DialogComponent {
-
+  readonly dialogRef = inject(MatDialogRef<DialogComponent>);
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
 }
